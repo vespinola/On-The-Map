4bb147b38aa1b75e645536389d1a141fb0c0511e
Vladimir Espinola <vladimir.espinola@gmail.com>
2017-12-18 19:15:38 -0300

    added project files

---
+++ OnTheMap/MapViewController.swift
@@ -0,0 +1,129 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by administrator on 12/18/17.
+//  Copyright © 2017 administrator. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        mapView.delegate = self
+        
+        var locations = hardCodedLocationData()
+        
+        var annotations:[MKAnnotation] = []
+        
+        for dictionary in locations {
+            
+            let lat = CLLocationDegrees(dictionary["latitude"] as! Double)
+            let long = CLLocationDegrees(dictionary["longitude"] as! Double)
+            
+            let coordinate = CLLocationCoordinate2D(latitude: lat, longitude: long)
+            
+            let first = dictionary["firstName"] as! String
+            let last = dictionary["lastName"] as! String
+            let mediaURL = dictionary["mediaURL"] as! String!
+            
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = coordinate
+            annotation.title = "\(first) \(last)"
+            annotation.subtitle = mediaURL
+            
+            annotations.append(annotation)
+            
+        }
+    }
+
+}
+
+
+extension MapViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let reuseId = "pin"
+        
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+        
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView?.canShowCallout = true
+            pinView?.pinColor = .red
+            pinView?.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        } else {
+            pinView?.annotation = annotation
+        }
+        
+        return pinView
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if control == view.rightCalloutAccessoryView {
+            let app = UIApplication.shared
+            if let toOpen = view.annotation?.subtitle {
+                app.openURL(URL(string: toOpen!)!)
+            }
+        }
+    }
+}
+
+extension MapViewController {
+    //MARK: Helpers
+    func hardCodedLocationData() -> [[String : Any]] {
+        return  [
+            [
+                "createdAt" : "2015-02-24T22:27:14.456Z",
+                "firstName" : "Jessica",
+                "lastName" : "Uelmen",
+                "latitude" : 28.1461248,
+                "longitude" : -82.75676799999999,
+                "mapString" : "Tarpon Springs, FL",
+                "mediaURL" : "www.linkedin.com/in/jessicauelmen/en",
+                "objectId" : "kj18GEaWD8",
+                "uniqueKey" : 872458750,
+                "updatedAt" : "2015-03-09T22:07:09.593Z"
+            ], [
+                "createdAt" : "2015-02-24T22:35:30.639Z",
+                "firstName" : "Gabrielle",
+                "lastName" : "Miller-Messner",
+                "latitude" : 35.1740471,
+                "longitude" : -79.3922539,
+                "mapString" : "Southern Pines, NC",
+                "mediaURL" : "http://www.linkedin.com/pub/gabrielle-miller-messner/11/557/60/en",
+                "objectId" : "8ZEuHF5uX8",
+                "uniqueKey" : 2256298598,
+                "updatedAt" : "2015-03-11T03:23:49.582Z"
+            ], [
+                "createdAt" : "2015-02-24T22:30:54.442Z",
+                "firstName" : "Jason",
+                "lastName" : "Schatz",
+                "latitude" : 37.7617,
+                "longitude" : -122.4216,
+                "mapString" : "18th and Valencia, San Francisco, CA",
+                "mediaURL" : "http://en.wikipedia.org/wiki/Swift_%28programming_language%29",
+                "objectId" : "hiz0vOTmrL",
+                "uniqueKey" : 2362758535,
+                "updatedAt" : "2015-03-10T17:20:31.828Z"
+            ], [
+                "createdAt" : "2015-03-11T02:48:18.321Z",
+                "firstName" : "Jarrod",
+                "lastName" : "Parkes",
+                "latitude" : 34.73037,
+                "longitude" : -86.58611000000001,
+                "mapString" : "Huntsville, Alabama",
+                "mediaURL" : "https://linkedin.com/in/jarrodparkes",
+                "objectId" : "CDHfAy8sdp",
+                "uniqueKey" : 996618664,
+                "updatedAt" : "2015-03-13T03:37:58.389Z"
+            ]
+        ]
+    }
+    
+}
