a3e661e5f2d0881bbf3f40f8c347ff30cb4db53f
Vladimir Espinola <vladimir.espinola@gmail.com>
2017-12-19 18:26:13 -0800

    updated main storyboard

---
+++ OnTheMap/Controller/RegisterLinkViewController.swift
@@ -0,0 +1,26 @@
+//
+//  RegisterLinkViewController.swift
+//  OnTheMap
+//
+//  Created by User on 12/19/17.
+//  Copyright © 2017 administrator. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class RegisterLinkViewController: UIViewController {
+    @IBOutlet var linkTextView: UIView!
+    @IBOutlet weak var map: MKMapView!
+    @IBOutlet var submitButton: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func submitButtonOnTap(_ sender: Any) {
+        
+    }
+}
