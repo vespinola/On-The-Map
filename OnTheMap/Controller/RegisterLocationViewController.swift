fbdc861e9b7fab9fc633b8d6eccbd3cafe55cc97
Vladimir Espinola <vladimir.espinola@gmail.com>
2017-12-19 17:47:51 -0800

    added register viewcontroller

---
+++ OnTheMap/Controller/RegisterLocationViewController.swift
@@ -0,0 +1,28 @@
+//
+//  RegisterLocationViewController.swift
+//  OnTheMap
+//
+//  Created by User on 12/19/17.
+//  Copyright © 2017 administrator. All rights reserved.
+//
+
+import UIKit
+
+class RegisterLocationViewController: UIViewController {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var locationTextView: UITextView!
+    @IBOutlet weak var registerButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func registerButtonOnTap(_ sender: Any) {
+        
+    }
+    
+    @IBAction func cancelButtonOnTap(_ sender: Any) {
+        
+    }
+}
---
+++ OnTheMap/Supporting Files/Util.swift
@@ -0,0 +1,13 @@
+//
+//  Util.swift
+//  OnTheMap
+//
+//  Created by User on 12/19/17.
+//  Copyright © 2017 administrator. All rights reserved.
+//
+
+import UIKit
+
+class Util: NSObject {
+
+}
