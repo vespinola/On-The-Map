d0fed5e82ab4ee21cd85c3ae4c746fd72af47081
Vladimir Espinola <vladimir.espinola@gmail.com>
2017-12-23 16:21:14 -0800

    listviewcontroller was finished

--- OnTheMap/Controller/ListViewController.swift
+++ OnTheMap/Controller/ListViewController.swift
@@ -9,10 +9,48 @@
 import UIKit
 
 class ListViewController: UIViewController {
+    @IBOutlet weak var listTableView: UITableView!
 
+    var students: [StudentLocation] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        students = ParseHandler.sharedInstance().studentsLocation
     }
+}
+
+extension ListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let student = students[indexPath.row]
+        
+        //from https://stackoverflow.com/a/39546889
+        guard let url = URL(string: student.mediaURL ?? "") else {
+            return //be safe
+        }
+        
+        if #available(iOS 10.0, *) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        } else {
+            UIApplication.shared.openURL(url)
+        }
+    }
+}
+
+extension ListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return students.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "StudentTableViewCell", for: indexPath)
+        let student = students[indexPath.row]
+        
+        cell.textLabel?.text = (student.firstName ?? "") + " " + (student.lastName ?? "")
+        cell.detailTextLabel?.text = student.mediaURL ?? ""
+        
+        return cell
+    }
+    
+    
 }
